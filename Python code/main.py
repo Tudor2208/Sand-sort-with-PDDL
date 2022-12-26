114235edc78ae8c86d4eaeaed81328c1f5d2d87f
Tudor <branettudor22@gmail.com>
2022-12-26 10:01:52 +0200

    project done

--- Python code/main.py
+++ Python code/main.py
@@ -28,15 +28,10 @@ bottle_4_1 = ""
 bottle_4_2 = ""
 bottle_4_3 = ""
 
-bottle_5_1 = ""
-bottle_5_2 = ""
-bottle_5_3 = ""
-
 colors_in_bottles = [
     [0, 0, 0],
     [0, 0, 0],
     [0, 0, 0],
-    ['white', 'white', 'white'],
     ['white', 'white', 'white']
 ]
 
@@ -155,14 +150,14 @@ def generate_game():
                 fill_bottle(j+1, i+1, "blue")
                 blue += 1
 
-    for i in range(4, 6):
-        fill_bottle(i, 1, 'white')
-        fill_bottle(i, 2, 'white')
-        fill_bottle(i, 3, 'white')
+    fill_bottle(4, 1, 'white')
+    fill_bottle(4, 2, 'white')
+    fill_bottle(4, 3, 'white')
 
     statements = get_init_statements()
     generate_problem_file('../part1', '../part2', statements)
     subprocess.check_call(['./fast-downward.py', 'domain_sand_sort.PDDL', 'problem_sand_sort2.PDDL', '--search', "astar(blind())"], cwd='../downward')
+    list_of_actions = []
     parse_plan_file('../downward/sas_plan')
 
 
@@ -230,16 +225,15 @@ def create_start_window():
     global bottle_2_1, bottle_2_2, bottle_2_3
     global bottle_3_1, bottle_3_2, bottle_3_3
     global bottle_4_1, bottle_4_2, bottle_4_3
-    global bottle_5_1, bottle_5_2, bottle_5_3
 
     window = Tk()
     window.title("Sand-sort-puzzle")
-    window.geometry("1300x800")
+    window.geometry("1200x800")
     window.resizable(False, False)
     window.configure(background='#ecf0ce')
     window_title = Label(text="Sort the sand!", font=('consolas', 30), background='#ecf0ce')
     window_title.pack(side=TOP)
-    my_canvas = tk.Canvas(window, bg="#ecf0ce", height=800, width=1300)
+    my_canvas = tk.Canvas(window, bg="#ecf0ce", height=800, width=1200)
 
     bottle_1_1 = my_canvas.create_rectangle(50, 150, 200, 230, outline="black", fill="white", width="2")
 
@@ -258,24 +252,17 @@ def create_start_window():
     bottle_4_2 = my_canvas.create_rectangle(650, 230, 800, 320, outline="black", fill="white", width="2")
     bottle_4_3 = my_canvas.create_rectangle(650, 320, 800, 400, outline="black", fill="white", width="2")
 
-    bottle_5_1 = my_canvas.create_rectangle(850, 150, 1000, 230, outline="black", fill="white", width="2")
-    bottle_5_2 = my_canvas.create_rectangle(850, 230, 1000, 320, outline="black", fill="white", width="2")
-    bottle_5_3 = my_canvas.create_rectangle(850, 320, 1000, 400, outline="black", fill="white", width="2")
-
     label_b1 = tk.Label(window, text="Bottle 1", font=("Times New Roman", 20), bg="#ecf0ce")
     label_b1.place(x="80", y="470")
 
     label_b2 = tk.Label(window, text="Bottle 2", font=("Times New Roman", 20), bg="#ecf0ce")
     label_b2.place(x="280", y="470")
 
-    label_b1 = tk.Label(window, text="Bottle 3", font=("Times New Roman", 20), bg="#ecf0ce")
-    label_b1.place(x="480", y="470")
+    label_b3 = tk.Label(window, text="Bottle 3", font=("Times New Roman", 20), bg="#ecf0ce")
+    label_b3.place(x="480", y="470")
 
-    label_b1 = tk.Label(window, text="Bottle 4", font=("Times New Roman", 20), bg="#ecf0ce")
-    label_b1.place(x="680", y="470")
-
-    label_b1 = tk.Label(window, text="Bottle 5", font=("Times New Roman", 20), bg="#ecf0ce")
-    label_b1.place(x="880", y="470")
+    label_b4 = tk.Label(window, text="Bottle 4", font=("Times New Roman", 20), bg="#ecf0ce")
+    label_b4.place(x="680", y="470")
 
     label_actions = tk.Label(window, text="Actions", font=("Times New Roman", 22), bg="#ecf0ce")
     label_actions.place(x="550", y="550")
@@ -284,10 +271,10 @@ def create_start_window():
     text_area.place(x="200", y="600")
 
     btn_solve = tk.Button(window, text="Solve", font=("Times New Roman", 22), command=do_actions, bg="#99baf0")
-    btn_solve.place(x="1125", y="250")
+    btn_solve.place(x="1025", y="250")
 
     btn_new_game = tk.Button(window, text="New game", font=("Times New Roman", 22), command=generate_game, bg="#99baf0")
-    btn_new_game.place(x="1100", y="350")
+    btn_new_game.place(x="1000", y="350")
 
     my_canvas.pack()
 
